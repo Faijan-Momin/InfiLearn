763d8a85144d74d303dc2aabc880f2aedcf190d8
Faijan-Momin <faijanmomin99@gmail.com>
2020-12-25 18:46:50 +0530

    First commit

--- js/script.js
+++ js/script.js
@@ -16,3 +16,20 @@ function closeNav() {
     document.getElementById("mobile-header-navbar").style.height = "0%";
   }
        
+
+//   Mobile Header Collapsible
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
