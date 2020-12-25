cdc5328f366249f4841418a7202ee830cf236745
Faijan-Momin <faijanmomin99@gmail.com>
2020-12-25 18:28:25 +0530

    First commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-// Mobile Header 
+// Mobile Header Navbar
+
+function MobileNav(){
+    if(document.getElementById("mobile-header-navbar").style.height == "100%"){
+        closeNav();
+    }
+    else{
+        openNav();
+    }
+}
 function openNav() {
     document.getElementById("mobile-header-navbar").style.height = "100%";
   }
   
-  function closeNav() {
+function closeNav() {
     document.getElementById("mobile-header-navbar").style.height = "0%";
   }
        
