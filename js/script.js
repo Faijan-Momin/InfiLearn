b38784863b1f8decd11060733140846120bebd97
Faijan-Momin <faijanmomin99@gmail.com>
2020-12-25 13:57:40 +0530

    First commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Mobile Header 
+function openNav() {
+    document.getElementById("mobile-header-navbar").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mobile-header-navbar").style.height = "0%";
+  }
+       
